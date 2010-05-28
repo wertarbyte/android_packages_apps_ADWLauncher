63ee0d793c5889389f06ed01c1d012f620da79f2
adw <adw@ubuntu-desktop.(none)>
2010-05-28 15:09:47 +0200

    fixed possible nullpointexception while trying to draw a children with null cache

--- src/com/android/launcher/AllAppsGridView.java
+++ src/com/android/launcher/AllAppsGridView.java
@@ -282,7 +282,11 @@ public class AllAppsGridView extends GridView implements AdapterView.OnItemClick
 				child.draw(canvas);
 			}
 		}else{
-			canvas.drawBitmap(cache, child.getLeft(), child.getTop(), mPaint);
+			if(cache!=null){
+				canvas.drawBitmap(cache, child.getLeft(), child.getTop(), mPaint);
+			}else{
+				child.draw(canvas);
+			}
 		}
 		canvas.restoreToCount(saveCount);
 		return true;
