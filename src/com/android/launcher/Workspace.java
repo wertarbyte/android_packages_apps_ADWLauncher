c8c374625dcaba705a7386856167545314dcb647
AnderWeb <anderweb@gmail.com>
2010-06-08 22:03:11 +0200

    Little fix for the first time you open the previews. Seems mScroller was never 'moved' to the default screen position

--- src/com/android/launcher/Workspace.java
+++ src/com/android/launcher/Workspace.java
@@ -582,6 +582,7 @@ public class Workspace extends ViewGroup implements DropTarget, DragSource, Drag
     	}
         if (mFirstLayout) {
             scrollTo(mCurrentScreen * width, 0);
+            mScroller.startScroll(0, 0, mCurrentScreen * width, 0, 0);
             if(lwpSupport)updateWallpaperOffset(width * (getChildCount() - 1));
             mFirstLayout = false;
         }
